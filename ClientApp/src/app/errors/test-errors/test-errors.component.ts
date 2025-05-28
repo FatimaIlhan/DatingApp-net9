1e27f006a03336002a5adf24aed15eb51486c1ec
Fatima Hassan Noor <fatimahnoor05@gmail.com>
2025-05-28 10:08:34 +0200

    validation errors and handling not found

--- ClientApp/src/app/errors/test-errors/test-errors.component.ts
+++ ClientApp/src/app/errors/test-errors/test-errors.component.ts
@@ -11,7 +11,7 @@ import { Component, inject } from '@angular/core';
 export class TestErrorsComponent {
 baseUrl = 'https://localhost:5001/';
 private http = inject(HttpClient)
-
+validationErrors: string [] = [];
 get400Error(){
   this.http.get(this.baseUrl + 'buggy/bad-request').subscribe({
     next: response => console.log(response),
@@ -50,7 +50,10 @@ get500Error(){
 get400ValidationError(){
   this.http.post(this.baseUrl + 'account/register',{}).subscribe({
     next: response => console.log(response),
-    error: error => console.log(error)
+    error: error =>{
+     console.log(error)
+     this.validationErrors =error;
+    }
     
 
   })
