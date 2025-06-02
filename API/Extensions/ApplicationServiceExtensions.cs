01b78cd53024ae5ba0af0e912883a3578f11f010
Fatima Hassan Noor <fatimahnoor05@gmail.com>
2025-06-02 16:06:23 +0200

    Creating Repository

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -8,17 +8,18 @@ namespace API.Extensions;
 
 public static class ApplicationServiceExtensions
 {
-    public static IServiceCollection AddApplicationService(this IServiceCollection services,
-    IConfiguration config)
-    {
-      services.AddControllers();
-      services.AddDbContext<DataContext>(opt =>
-        {
-            opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
-        });
-      services.AddCors();
-      services.AddScoped<ITokenService, TokenService>();
+  public static IServiceCollection AddApplicationService(this IServiceCollection services,
+  IConfiguration config)
+  {
+    services.AddControllers();
+    services.AddDbContext<DataContext>(opt =>
+      {
+        opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+      });
+    services.AddCors();
+    services.AddScoped<ITokenService, TokenService>();
+    services.AddScoped<IUserRepository, UserRepository>();
 
-      return services;
-    }
+    return services;
+  }
 }
