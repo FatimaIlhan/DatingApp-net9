fc16ec1736708a0db58fd1726c891e3dd920424d
Fatima Hassan Noor <fatimahnoor05@gmail.com>
2025-06-02 18:55:09 +0200

    Using AutoMapper

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,7 +1,9 @@
 using System;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,25 +11,26 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers;
 
 [Authorize]
-public class UsersController(IUserRepository userRepository) : BaseApiController
+public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
 
 {
 
 
     [HttpGet]
 
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
     {
         // get list of users
         var users = await userRepository.GetUsersAsync();
+        var usersToReturn = mapper.Map<IEnumerable<MemberDto>>(users);
         //return list of users to client
-        return Ok(users);
+        return Ok(usersToReturn);
     }
 
 
     [HttpGet("{username}")]
 
-    public async Task<ActionResult<AppUser>> GetUser(string username)
+    public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
         // get individual user  
         var user = await userRepository.GetUserByUsernameAsync(username);
@@ -35,6 +38,6 @@ public class UsersController(IUserRepository userRepository) : BaseApiController
         if (user == null) return NotFound();
         //return individual  user to client
         //return Ok(user);
-        return user;
+        return mapper.Map<MemberDto>(user);
     }
 }
