9ef6de544ec33dc207675b2b0fe40a9d604b9278
Fatima Hassan Noor <fatimahnoor05@gmail.com>
2025-06-02 18:08:05 +0200

    Updating the User Controller

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,37 +1,40 @@
 using System;
 using API.Data;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class UsersController(DataContext context) : BaseApiController
+[Authorize]
+public class UsersController(IUserRepository userRepository) : BaseApiController
 
 {
-   
-    [AllowAnonymous]
+
+
     [HttpGet]
 
-   public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
-   {
-    // get list of users
-    var users =  await context.Users.ToListAsync();
-    //return list of users to client
-    return Ok(users);
-   }
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        // get list of users
+        var users = await userRepository.GetUsersAsync();
+        //return list of users to client
+        return Ok(users);
+    }
 
-    [Authorize]
-    [HttpGet("{id:int}")]
 
-   public async Task<ActionResult<AppUser>> GetUser(int id)
-   {
-    // get individual user  
-    var user = await context.Users.FindAsync(id);
-    //check if we have user
-    if(user == null) return NotFound();
-    //return individual  user to client
-    return Ok(user);
-   }
+    [HttpGet("{username}")]
+
+    public async Task<ActionResult<AppUser>> GetUser(string username)
+    {
+        // get individual user  
+        var user = await userRepository.GetUserByUsernameAsync(username);
+        //check if we have user
+        if (user == null) return NotFound();
+        //return individual  user to client
+        //return Ok(user);
+        return user;
+    }
 }
